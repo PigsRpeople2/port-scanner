909def7ef5a00fe7a6db34ad1b204e85e70a4337
PigsRpeople2 <167029562+PigsRpeople2@users.noreply.github.com>
2026-01-17 03:13:58 +1100

    added basic OS detection
    Cant confirm the estimated OS output will be correct yet

--- port-scanner.py
+++ port-scanner.py
@@ -2,6 +2,49 @@ import requests
 import threading
 import time
 import argparse
+from scapy.all import ICMP, IP, sr1, traceroute, TCP, sr
+
+
+def os_scan(target_ip):
+    icmp_packet = IP(dst=target_ip)/ICMP()
+    
+    response = sr1(icmp_packet, timeout=2, verbose=0)
+    
+    result, unanswered = traceroute(target_ip, verbose=0, timeout=2)
+    tracedata = result.get_trace()
+    
+    est_ttl = len(tracedata) + icmp_packet.ttl - 1
+    
+
+
+    tcp_packet = IP(dst=target_ip)/TCP(dport=80, flags='S')
+    est_window = tcp_packet.window
+    
+    est_os = "Unknown"
+    if est_ttl <= 64:
+        if est_window == 5840 or est_window == 5720:
+            est_os = "Linux (Kernel 2.4/2.6/customized)"
+        elif est_window == 65535:
+            est_os = "FreeBSD/OpenBSD/NetBSD"
+    elif 65 <= est_ttl <= 128:
+        if est_window == 65535:
+            est_os = "Windows XP/8/10/Server 2003+"
+        elif est_window == 8192:
+            est_os = "Windows 7/Vista"
+    elif 129 <= est_ttl <= 255:
+        if est_window == 4128:
+            est_os = "Cisco Router (IOS 12.4+)"
+        elif est_window == 8760:
+            est_os = "Solaris 10/11"
+    
+    
+    
+    
+    
+    return {"est_ttl": est_ttl, "est_window": est_window, "os": est_os}
+    
+
+
 
 
 
@@ -61,42 +104,80 @@ parser = argparse.ArgumentParser(description="Basic Port Scanner")
 parser.add_argument("target", help="The target IP address to scan.", nargs='?')
 parser.add_argument('--ports', '-p', type=str, help='The range of ports to scan (e.g., "20-80" or "1000"), default = 10000', default="10000")
 parser.add_argument('--threads', '-t', type=int, help='Max threads to use, default = 100', default=100)
+parser.add_argument('--os-detection', '-o', action='store_true', help='Enable OS detection, default = False', default=False)
+parser.add_argument('--port-scan', '-P', action='store_false', help='Enable port scanning, default = True', default=True)
 
 
 
 args = parser.parse_args()
 
+run_port_scan = None
+open_ports = None
+run_os_scan = None
+os_details = None
+
 if args.target:
 
-    if '-' in args.ports:
-        start_port, end_port = map(int, args.ports.split('-'))
-        port_range = [start_port, end_port]
-    else:
-        port_range = int(args.ports)
+    if args.os_detection:
+        os_details = os_scan(args.target)
+        run_os_scan = True
 
-    open_ports = scan_ports(args.target, port_range, args.threads)
+    if args.port_scan:
+        if '-' in args.ports:
+            start_port, end_port = map(int, args.ports.split('-'))
+            port_range = [start_port, end_port]
+        else:
+            port_range = int(args.ports)
 
-    if open_ports:
-        print(f"Open ports: {open_ports}")
-    else:
-        print("No open ports found.")
+        open_ports = scan_ports(args.target, port_range, args.threads)
+        run_port_scan = True
+
+        
+        
         
 if not args.target:
     target = input("Target IP address: ")
-    port_start = input("Starting port (default 1): ")
-    port_end = input("Ending port (default 10000): ")
-    threads = input("Max threads (default 100): ")
-    if not threads:
-        threads = 100
-    if not port_start:
-        port_start = 1
-    if not port_end:
-        port_end = 10000
+    port_scan = input("Enable port scanning? (y/n, default y): ")
+    
+    port_start = None
+    port_end = None
+    threads = None
+    if port_scan.lower() != 'n':
+        port_start = input("Starting port (default 1): ")
+        port_end = input("Ending port (default 10000): ")
+        threads = input("Max threads (default 100): ")
+        if not threads:
+            threads = 100
         
-    open_ports = scan_ports(target, [int(port_start), int(port_end)], int(threads))
+        if not port_start:
+            port_start = 1
+        
+        if not port_end:
+            port_end = 10000
+    
+    
+    os_detection = input("Enable OS detection? (y/n, default n): ")
+    
+    
+    
+    if os_detection.lower() == 'y':
+        os_details = os_scan(target)
+        run_os_scan = True
+        
+    if port_scan.lower() != 'n':
+        open_ports = scan_ports(target, [int(port_start), int(port_end)], int(threads))
+        run_port_scan = True
+    
+    
+if run_port_scan:
     if open_ports:
         print(f"Open ports: {open_ports}")
     else:
         print("No open ports found.")
-        
+
+if run_os_scan:
+    if os_details:
+        print(f"Initial TTL: {os_details['est_ttl']}")
+        print(f"Window Size: {os_details['est_window']}")
+        print(f"Estimated OS: {os_details['os']}")
     
